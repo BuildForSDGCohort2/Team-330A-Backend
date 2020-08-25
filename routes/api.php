7e3e408e9a9bbf36731fb1eed4c62e40b5f85985
franklin <uhexos@gmail.com>
2020-08-25 23:39:38 +0000

    added passport, login&register routes

--- routes/api.php
+++ routes/api.php
@@ -12,8 +12,13 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::post('/register', 'Api\AuthController@register');
+Route::post('/login', 'Api\AuthController@login');
+Route::get('/test', function () {
+    return 'Hello World';
 });
